14eb4a6a6fc968353e9015c3f33fe67847b9db1a
Xurvan <xurvan@tuta.io>
2020-09-15 09:21:55 +0430

    Remove methods side effects and make functions pure

--- fancy/mixins.py
+++ fancy/mixins.py
@@ -6,7 +6,7 @@ class _BaseSerializer(ModelSerializer):
     class Meta:
         model: models.Model
 
-    def _extract_relations(self) -> tuple:
+    def _extract_relations(self, validated_data) -> tuple:
         many_to_many = dict()
         one_to_many = dict()
 
@@ -29,28 +29,28 @@ class _BaseSerializer(ModelSerializer):
                 pass
 
         for field_name in many_to_many:
-            self.validated_data.pop(field_name)
-
-        self.many_to_many_data = many_to_many
-        self.one_to_many_data = one_to_many
+            validated_data.pop(field_name)
 
         return many_to_many, one_to_many
 
-    def _save_none_relational_fields(self):
-        self.instance = self.Meta.model.objects.create(**self.validated_data)
+    def _save_none_relational_fields(self, validated_data):
+        instance = self.Meta.model.objects.create(**validated_data)
 
-        return self.instance
+        self.instance = instance
+        return instance
 
-    def _update_none_relational_fields(self):
-        for field_name, field_value in self.validated_data.items():
-            setattr(self.instance, field_name, field_value)
-        self.instance.save()
+    def _update_none_relational_fields(self, instance, validated_data):
+        for field_name, field_value in validated_data.items():
+            setattr(instance, field_name, field_value)
+        instance.save()
 
-        return self.instance
+        self.instance = instance
+        return instance
 
-    def _save_or_update_many_to_many_fields(self, update=True) -> None:
-        for field_name, field_value in self.many_to_many_data.items():
-            attr = getattr(self.instance, field_name)
+    @staticmethod
+    def _save_or_update_many_to_many_fields(instance, many_to_many_data, update=True) -> None:
+        for field_name, field_value in many_to_many_data.items():
+            attr = getattr(instance, field_name)
 
             if update:
                 attr.clear()
@@ -58,11 +58,11 @@ class _BaseSerializer(ModelSerializer):
             for pk in field_value:
                 attr.add(pk)
 
-    def _save_many_to_many_fields(self) -> None:
-        self._save_or_update_many_to_many_fields()
+    def _save_many_to_many_fields(self, instance, many_to_many_data) -> None:
+        self._save_or_update_many_to_many_fields(instance, many_to_many_data)
 
-    def _update_many_to_many_fields(self) -> None:
-        self._save_or_update_many_to_many_fields(update=True)
+    def _update_many_to_many_fields(self, instance, many_to_many_data) -> None:
+        self._save_or_update_many_to_many_fields(instance, many_to_many_data, update=True)
 
     def _save_one_to_many_fields(self):
         raise NotImplemented()
@@ -70,17 +70,17 @@ class _BaseSerializer(ModelSerializer):
 
 class FancyCreateMixin(_BaseSerializer):
     def create(self, validated_data):
-        self._extract_relations()
-        self._save_none_relational_fields()
-        self._save_many_to_many_fields()
+        many_to_many, one_to_many = self._extract_relations(validated_data)
+        instance = self._save_none_relational_fields(validated_data)
+        self._save_many_to_many_fields(instance, many_to_many)
 
         return self.instance
 
 
 class FancyUpdateMixin(_BaseSerializer):
     def update(self, instance, validated_data):
-        self._extract_relations()
-        self._update_many_to_many_fields()
-        self._update_none_relational_fields()
+        many_to_many, one_to_many = self._extract_relations(validated_data)
+        self._update_many_to_many_fields(instance, many_to_many)
+        self._update_none_relational_fields(instance, validated_data)
 
         return self.instance
