a32649cf4cc6c46333f99139bd35515fd7e2004d
Xurvan <xurvan@tuta.io>
2020-09-16 20:45:49 +0430

    I know! Side effects are bad. But it looks beautiful ^_^

--- fancy/mixins.py
+++ fancy/mixins.py
@@ -8,7 +8,7 @@ class _BaseSerializer(ModelSerializer):
     class Meta:
         model: models.Model
 
-    def _prepare_relational_fields(self, validated_data) -> dict:
+    def _prepare_relational_fields(self) -> None:
         many_to_many = defaultdict(list)
 
         for field_name, field_value in self.initial_data.items():
@@ -22,32 +22,25 @@ class _BaseSerializer(ModelSerializer):
                 many_to_many[_field_name] += field_value
             elif isinstance(field, ModelSerializer):
                 obj = field.Meta.model.objects.create(**field_value)
-                validated_data[field_name + '_id'] = obj.pk
-                validated_data.pop(field_name)
+                self.validated_data[field_name + '_id'] = obj.pk
+                self.validated_data.pop(field_name)
 
+        self.many_to_many_data = many_to_many
         for field_name in many_to_many:
-            validated_data.pop(field_name)
+            self.validated_data.pop(field_name)
 
-        return many_to_many
-
-    def _save_none_relational_fields(self, validated_data):
-        instance = self.Meta.model.objects.create(**validated_data)
-
+    def _save_none_relational_fields(self) -> None:
+        instance = self.Meta.model.objects.create(**self.validated_data)
         self.instance = instance
-        return instance
 
-    def _update_none_relational_fields(self, instance, validated_data):
-        for field_name, field_value in validated_data.items():
-            setattr(instance, field_name, field_value)
-        instance.save()
+    def _update_none_relational_fields(self) -> None:
+        for field_name, field_value in self.validated_data.items():
+            setattr(self.instance, field_name, field_value)
+        self.instance.save()
 
-        self.instance = instance
-        return instance
-
-    @staticmethod
-    def _save_or_update_many_to_many_fields(instance, many_to_many_data, update=True) -> None:
-        for field_name, field_value in many_to_many_data.items():
-            attr = getattr(instance, field_name)
+    def _save_or_update_many_to_many_fields(self, update=True) -> None:
+        for field_name, field_value in self.many_to_many_data.items():
+            attr = getattr(self.instance, field_name)
 
             if update:
                 attr.clear()
@@ -55,26 +48,26 @@ class _BaseSerializer(ModelSerializer):
             for pk in field_value:
                 attr.add(pk)
 
-    def _save_many_to_many_fields(self, instance, many_to_many_data) -> None:
-        self._save_or_update_many_to_many_fields(instance, many_to_many_data)
+    def _save_many_to_many_fields(self) -> None:
+        self._save_or_update_many_to_many_fields()
 
-    def _update_many_to_many_fields(self, instance, many_to_many_data) -> None:
-        self._save_or_update_many_to_many_fields(instance, many_to_many_data, update=True)
+    def _update_many_to_many_fields(self) -> None:
+        self._save_or_update_many_to_many_fields(update=True)
 
 
 class FancyCreateMixin(_BaseSerializer):
     def create(self, validated_data):
-        many_to_many = self._prepare_relational_fields(validated_data)
-        instance = self._save_none_relational_fields(validated_data)
-        self._save_many_to_many_fields(instance, many_to_many)
+        self._prepare_relational_fields()
+        self._save_none_relational_fields()
+        self._save_many_to_many_fields()
 
         return self.instance
 
 
 class FancyUpdateMixin(_BaseSerializer):
     def update(self, instance, validated_data):
-        many_to_many = self._prepare_relational_fields(validated_data)
-        self._update_many_to_many_fields(instance, many_to_many)
-        self._update_none_relational_fields(instance, validated_data)
+        self._prepare_relational_fields()
+        self._update_many_to_many_fields()
+        self._update_none_relational_fields()
 
         return self.instance
