eee9d0ce05226430b6a46cda208fa62c6a855b0c
Xurvan <xurvan@tuta.io>
2020-10-15 20:55:40 +0330

    To avoid confusion start filter parameters with "__" and convert numerical values

--- fancy/viewsets.py
+++ fancy/viewsets.py
@@ -3,4 +3,17 @@ from rest_framework import viewsets
 
 class FancyViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
-        return self.serializer_class.Meta.model.objects.filter(**self.request.query_params.dict())
+        params = {}
+        for param in self.request.query_params:
+            # To avoid confusion we start our filter parameters with "__"
+            if not param.startswith('__'):
+                continue
+            key = param[2:]
+
+            # Django "filter" dose not support numerical conversion for JSON fields. We convert all params ourself
+            try:
+                params[key] = float(self.request.query_params[param])
+            except ValueError:
+                params[key] = self.request.query_params[param]
+
+        return self.serializer_class.Meta.model.objects.filter(**params)
