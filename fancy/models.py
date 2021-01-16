91bed30d9f27f9c7134240df287a5f299a250e59
Xurvan <xurvan@tuta.io>
2021-01-16 11:22:18 +0330

    Add "updated_at" field to our fancy model

--- fancy/models.py
+++ fancy/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 class FancyModel(models.Model):
     inserted_at = models.DateTimeField(auto_now_add=True, null=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now_add=True, null=True, db_index=True)
     deleted_at = models.DateTimeField(null=True, db_index=True)
 
     class Meta:
@@ -12,3 +13,11 @@ class FancyModel(models.Model):
     def delete(self, using=None, keep_parents=False):
         self.deleted_at = timezone.now()
         self.save()
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.updated_at = timezone.now()
+        return super(FancyModel, self).save(
+            force_insert=force_insert,
+            force_update=force_update,
+            using=using,
+            update_fields=update_fields)
