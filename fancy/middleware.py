d5706672a72f7bdba177bb69132f676b2c35e336
Xurvan <xurvan@tuta.io>
2021-01-27 15:26:03 +0330

    Add elapsed time middleware

--- fancy/middleware.py
+++ fancy/middleware.py
@@ -1,5 +1,19 @@
+import time
+
 from django.conf import settings
 from django.http import JsonResponse
+
+
+class ElapsedMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        start_time = time.process_time()
+        response = self.get_response(request)
+        response['X-Elapsed-Time'] = time.process_time() - start_time
+
+        return response
 
 
 class ErrorHandlerMiddleware:
