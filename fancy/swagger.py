0b9102ed65bde6b8b5ee54256139f1fc02a6afa9
Xurvan <xurvan@tuta.io>
2021-01-30 19:04:02 +0330

    Inherit from "FieldInspector" instead of "InlineSerializerInspector"

--- fancy/swagger.py
+++ fancy/swagger.py
@@ -1,16 +1,19 @@
 from drf_yasg.app_settings import swagger_settings
-from drf_yasg.inspectors import SwaggerAutoSchema, InlineSerializerInspector
+from drf_yasg.inspectors import SwaggerAutoSchema, InlineSerializerInspector, FieldInspector
 
 from fancy.fields import LoginRequiredSerializerField, SelfSerializerField
 
 
-class FancyInspector(InlineSerializerInspector):
+class FancyInspector(FieldInspector):
     def field_to_swagger_object(self, field, swagger_object_type, use_references, **kwargs):
         if isinstance(field, (LoginRequiredSerializerField, SelfSerializerField)):
             field = field.serializer(many=field.many, read_only=True)
             use_references = True
 
-        return super(FancyInspector, self).field_to_swagger_object(
+        inspector = InlineSerializerInspector(view=self.view, path=self.path, method=self.method,
+                                              components=self.components, request=self.request,
+                                              field_inspectors=self.field_inspectors)
+        return inspector.field_to_swagger_object(
             field=field,
             swagger_object_type=swagger_object_type,
             use_references=use_references,
@@ -18,4 +21,4 @@ class FancyInspector(InlineSerializerInspector):
 
 
 class FancyAutoSchema(SwaggerAutoSchema):
-    field_inspectors = [*swagger_settings.DEFAULT_FIELD_INSPECTORS, FancyInspector]
+    field_inspectors = [FancyInspector, *swagger_settings.DEFAULT_FIELD_INSPECTORS]
