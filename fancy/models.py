04c9936c7f205df775b7532510b80df71380a1af
Xurvan <xurvan@tuta.io>
2021-02-08 10:24:45 +0330

    Replace update mechanism with "auto_now" arguments

--- fancy/models.py
+++ fancy/models.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 
 
 class FancyModel(models.Model):
-    inserted_at = models.DateTimeField(auto_now_add=True, null=True, db_index=True)
-    updated_at = models.DateTimeField(auto_now_add=True, null=True, db_index=True)
+    inserted_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, db_index=True)
 
     class Meta:
@@ -13,11 +13,3 @@ class FancyModel(models.Model):
     def delete(self, using=None, keep_parents=False):
         self.deleted_at = timezone.now()
         self.save()
-
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        self.updated_at = timezone.now()
-        return super(FancyModel, self).save(
-            force_insert=force_insert,
-            force_update=force_update,
-            using=using,
-            update_fields=update_fields)
