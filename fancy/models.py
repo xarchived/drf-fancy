c28dba2216823ec48747ffd1cfe82c9cbf2f00f7
Xurvan <xurvan@tuta.io>
2021-06-12 20:55:19 +0430

    Enable "db_index" on "updated_at" field

--- fancy/models.py
+++ fancy/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class FancyModel(models.Model):
     inserted_at = models.DateTimeField(auto_now_add=True, db_index=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
     deleted_at = models.DateTimeField(null=True, db_index=True)
 
     class Meta:
