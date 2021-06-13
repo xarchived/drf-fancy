533ac3e46834d5a18b7f69d503edadda19aa132c
Xurvan <xurvan@tuta.io>
2021-06-13 11:07:20 +0430

    Split "FancyModel" into "SafeDeleteModel" and "LogFieldsModel"

--- fancy/models.py
+++ fancy/models.py
@@ -2,9 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 
-class FancyModel(models.Model):
-    inserted_at = models.DateTimeField(auto_now_add=True, db_index=True)
-    updated_at = models.DateTimeField(auto_now=True, db_index=True)
+class SafeDeleteModel(models.Model):
     deleted_at = models.DateTimeField(null=True, db_index=True)
 
     class Meta:
@@ -13,3 +11,11 @@ class FancyModel(models.Model):
     def delete(self, using=None, keep_parents=False):
         self.deleted_at = timezone.now()
         self.save()
+
+
+class LogFieldsModel(models.Model):
+    inserted_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
+
+    class Meta:
+        abstract = True
