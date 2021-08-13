4afc0cd57a7fe6df859699dc1fe3176e1826fa4f
Xurvan <xurvan@tutanota.com>
2021-08-13 13:49:51 +0430

    Add "DeletedManager" and "UndeletedManager" managers

--- fancy/models.py
+++ fancy/models.py
@@ -1,9 +1,23 @@
-from django.db.models import Model, DateTimeField
+from django.db.models import Model, DateTimeField, Manager
 from django.utils import timezone
+
+
+class UndeletedManager(Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at__isnull=True)
+
+
+class DeletedManager(Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at__isnull=False)
 
 
 class SafeDeleteModel(Model):
     deleted_at = DateTimeField(null=True, db_index=True)
+
+    objects = Manager()
+    deleted_objects = DeletedManager()
+    undeleted_objects = UndeletedManager()
 
     class Meta:
         abstract = True
